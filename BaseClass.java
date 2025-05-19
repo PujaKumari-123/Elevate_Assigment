f61db6745d0de4eb4b0bf2a9ff924711d33b5595
Puja Kumari <puja291196m@gmail.com>
2025-05-19 18:36:49 +0530

    Add files via upload

---
+++ BaseClass.java
@@ -0,0 +1,32 @@
+package baseClass;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass {
+
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	
+	public void setup() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	
+ @AfterMethod
+ public void tearDown() throws InterruptedException {
+	  
+	 Thread.sleep(2000);
+	 driver.quit();
+ }
+	
+	
+}
